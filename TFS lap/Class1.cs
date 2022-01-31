b8f5b23ae9ceefe971d146e9a2275eb9be507165
Mahran Mohamed <mahran_mahran97@yahoo.com>
2022-01-31 12:53:53 +0200

    add  class "Add"

---
+++ TFS lap/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TFS_lap
+{
+    internal class Add
+    {
+        public string Name = "Mahran ";
+    }
+}
