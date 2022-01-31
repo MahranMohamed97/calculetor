206de9e0e2fceaba16ee63e6173f9b4c0185ae2f
Mahran <Mahran@DESKTOP-TEMRBPD>
2022-01-31 12:12:33 +0200

    Add project files.

---
+++ TFS lap/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TFS_lap
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
